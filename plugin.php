26c9ae30774046e8d32344053fccc48bb3bbeabe
Marco Carraro <carra.rules@gmail.com>
2023-11-30 18:17:49 +0100

    Create plugin header

---
+++ plugin.php
@@ -0,0 +1,34 @@
+<?php /**
+* Plugin Name: Mobile Cover Extension
+* Plugin URI:
+* Description: Upload a mobile friendly image for the Cover Block
+* Version: 1.0.0
+* Requires at least: 5.3
+* Requires PHP: 5.3
+* Author: MC
+* Author URI: https://theunwrappedoptional.github.io/theunwrappedoptional/
+* License: GPL v3 or later
+* License URI: https://www.gnu.org/licenses/gpl-3.0.html
+* Text Domain: mobile-cover-extension
+* Domain Path:
+*/
+
+/*
+	Upload a mobile friendly image for the Cover Block.
+
+    Copyright (C) <2023>  <MC>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
