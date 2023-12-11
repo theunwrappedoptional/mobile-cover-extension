b345316c68b8d4ed0b06a39ec43b0218a65f2f8f
Marco Carraro <carra.rules@gmail.com>
2023-12-11 12:36:32 +0100

    Fix Fixed Cover Image behaviour with css

--- plugin.php
+++ plugin.php
@@ -33,8 +33,6 @@
 
 */
 
-add_action('enqueue_block_editor_assets', 'enqueue_mobile_cover_extension', 100);
-
 function enqueue_mobile_cover_extension() {
   
   $dir = plugin_dir_url( __FILE__ );
@@ -45,18 +43,27 @@ function enqueue_mobile_cover_extension() {
   wp_enqueue_script('mobile-cover', $dir.'js/mobile-cover.js', [ 'wp-blocks', 'wp-dom' ] , null, true);
 }
 
-/*
- Update 2023) As of latest WordPress, if you set the Cover to “Fixed Background”, the <img> will be replaced with <div role="img">. That means we can no longer uses <picture>.
+add_action('enqueue_block_editor_assets', 'enqueue_mobile_cover_extension', 100);
 
- The code below has conditional to check that. If using <div>, it will add extra CSS variable instead of wrapping it in <picture>
+function enqueue_mobile_cover_css() {
+	wp_enqueue_style( 'mobile-cover-style', plugin_dir_url( __FILE__ ).'css/mobile-cover-style.css');
+}
+
+add_action( 'wp_enqueue_scripts', 'enqueue_mobile_cover_css' );
+
+/*
+	- UPDATE 2023 -
+	As of latest WordPress, if you set the Cover to “Fixed Background”, the <img> will be replaced with <div role="img">. That means we can no longer uses <picture>.
+	The code below has conditional to check that. If using <div>, it will add extra CSS variable instead of wrapping it in <picture>
 */
 
 add_filter('render_block_core/cover', 'my_responsive_cover_render', 10, 2);
 
 function my_responsive_cover_render($content, $block) {
   // If has mobile image
-  if (isset($block['attrs']['mobileImageURL'])) {
-    $image = $block['attrs']['mobileImageURL'];
+  if ((isset($block['attrs']['mobileImageURL'])) && ($block['attrs']['mobileImageURL'] != '')) {
+    
+	$image = $block['attrs']['mobileImageURL'];
 
     preg_match('/<div role="img"/', $content, $is_fixed);
 
